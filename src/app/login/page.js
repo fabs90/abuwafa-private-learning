6b6f32f66c9a0732ebca282f542fb68cfce8d290
fabian <fabianjuliansyah89@gmail.com>
2025-01-21 14:55:45 +0700

    Fix/Wrap login page with suspensee

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,10 +1,13 @@
 import FormLogin from "@/components/login/FormLogin";
 import "../../components/login/formLogin.css";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
     <div className="flex justify-center items-center h-screen mx-6 md:mx-0 lg:mx-0">
-      <FormLogin />
+      <Suspense>
+        <FormLogin />
+      </Suspense>
     </div>
   );
 }
